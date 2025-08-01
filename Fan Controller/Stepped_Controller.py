19eaa9b5e0ad5d9d9cedcd72dc67b8b35ceb1f9c
Saif-Aldain Aqel <saifaqel3@gmail.com>
2025-08-01 13:24:09 +0200

    Update Stepped_Controller.py

--- Fan Controller/Stepped_Controller.py
+++ Fan Controller/Stepped_Controller.py
@@ -1,18 +1,12 @@
-# Created by: Michael Klements
-# For 40mm 5V PWM Fan Control On A Raspberry Pi
-# Sets fan speed in stepped increments - better for low quality fans
-# Works well with a Pi Desktop Case with OLED Stats Display
-# Installation & Setup Instructions - https://www.the-diy-life.com/connecting-a-pwm-fan-to-a-raspberry-pi/
-
-#import RPi.GPIO as IO          # Calling GPIO to allow use of the GPIO pins
+import RPi.GPIO as IO          # Calling GPIO to allow use of the GPIO pins
 import time                    # Calling time to allow delays to be used
 import subprocess              # Calling subprocess to get the CPU temperature
 
-#IO.setwarnings(False)          # Do not show any GPIO warnings
-#IO.setmode (IO.BCM)            # BCM pin numbers - PIN8 as ‘GPIO14’
-#IO.setup(14,IO.OUT)            # Initialize GPIO14 as our fan output pin
-#fan = IO.PWM(14,100)           # Set GPIO14 as a PWM output, with 100Hz frequency (this should match your fans specified PWM frequency)
-#fan.start(0)                   # Generate a PWM signal with a 0% duty cycle (fan off)
+IO.setwarnings(False)          # Do not show any GPIO warnings
+IO.setmode (IO.BCM)            # BCM pin numbers - PIN8 as ‘GPIO14’
+IO.setup(14,IO.OUT)            # Initialize GPIO14 as our fan output pin
+fan = IO.PWM(14,100)           # Set GPIO14 as a PWM output, with 100Hz frequency (this should match your fans specified PWM frequency)
+fan.start(0)                   # Generate a PWM signal with a 0% duty cycle (fan off)
 
 def get_temp():                              # Function to read in the CPU temperature and return it as a float in degrees celcius
     output = subprocess.run(['vcgencmd', 'measure_temp'], capture_output=True)
